32c4aa01abdd592279990f53796f51e21e642e2e
FaryalFaisal29 <164797311+FaryalFaisal29@users.noreply.github.com>
2024-03-25 02:05:44 +0500

    this contains modules

---
+++ index.js
@@ -0,0 +1,10 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+let answer = await inquirer.prompt([{
+        name: "age",
+        type: "number",
+        message: "Enter your Age:"
+    }]);
+console.log(chalk.red("In Sha Allah, in " + (60 - answer.age) + "years you will be 60 years old."));
+console.log(chalk.green("Hello,") + chalk.blue("World"));
+console.log(chalk.red.bold("This is an Error message!"));
